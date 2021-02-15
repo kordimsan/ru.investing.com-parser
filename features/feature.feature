e921a8fb924440d9ef08ec556735330618147190
Korzunin Dmitry <dakorzunin@sberbank.ru>
2021-02-15 15:41:49 +0500

    Поправил тесты запуском из класса

--- features/feature.feature
+++ features/feature.feature
@@ -1,10 +1,9 @@
 Feature: Парсинг ru.investing.com
 Scenario: Открытие сайта ru.investing.com
-  Given Запущен драйвер chromedriver
-  Then Переходим по адресу 'https://ru.investing.com/'
+  Given Переходим по адресу 'https://ru.investing.com/'
   Then Переходим в меню 'Акции'
   Then Переходим в меню 'Россия'
   Then Проверяем заголовок по имени 'Россия - акции'
   Then Получаем таблицу с котировками с сайта
-  Then Получаем таблицу с котировками из базы 'D:\Desktop\Задания\stocks'
+  Then Получаем таблицу с котировками из базы 'stocks'
   Then Получаем таблицу котировок цена которых изменилась на 50%
--- features/steps/test.py
+++ features/steps/test.py
@@ -1,75 +1,32 @@
 # -*- coding: utf-8 -*-
-import locale
-import sqlite3
 import time
 from pprint import pprint
-
+import my_parser as parser
 from behave import *
-from selenium import webdriver
-
-locale.setlocale(locale.LC_ALL, 'nl_NL')
-
-option = webdriver.ChromeOptions()
-prefs = {'profile.default_content_setting_values': {'images':2, 'javascript':2}}
-option.add_experimental_option('prefs', prefs)
 
 class TextNotFound(Exception):
     pass
 
-def read_sqlite_table(base_path):
-    try:
-        sqlite_connection= sqlite3.connect(base_path, timeout=20)
-        cursor = sqlite_connection.cursor()
-        print("Подключен к SQLite")
-
-        sqlite_select_query = """SELECT * from stock_price"""
-        cursor.execute(sqlite_select_query)
-        rows = cursor.fetchall()
-        return rows
-        cursor.close()
-
-    except sqlite3.Error as error:
-        print("Ошибка при подключении к sqlite", error)
-    finally:
-        if (sqlite_connection):
-            sqlite_connection.close()
-            print("Соединение с SQLite закрыто")
-
-
-@given('Запущен драйвер chromedriver')
-def step(context):
-    context.browser = webdriver.Chrome("C:\\chromedriver.exe", options=option)
-
-@then("Переходим по адресу '{url}'")
+@given("Переходим по адресу '{url}'")
 def step(context, url):
-    context.browser.get(url)
+    context.browser = parser.InvestingParse(url)
 
 @then("Переходим в меню '{text}'")
 def step(context, text):
-    menu = context.browser.find_element_by_xpath(f"//a[contains(text(),'{text}')]")
-    link = menu.get_attribute('href')
-    context.browser.get(link)
+    context.browser._get_menu(text)
 
 @then("Проверяем заголовок по имени '{name}'")
 def step(context, name):
-    elements = context.browser.find_elements_by_xpath("//section[@id='leftColumn']//h1")
-    if not elements[0].get_attribute('innerText') == name:
+    if not context.browser._is_target(name):
         raise TextNotFound("Не найден заголовок '{name}'")
 
 @then("Получаем таблицу с котировками с сайта")
 def step(context):
-    elements = context.browser.find_elements_by_xpath("//table[@id='cross_rate_markets_stocks_1']//tbody//tr")
-    data = []
-    for tr in elements:
-        tds = tr.find_elements_by_tag_name('td')
-        if tds: 
-            data.append((tds[1].text,tds[2].text))
-    context.data = [(a,locale.atof(b)) for a,b in data]
+    context.data = context.browser.get_prices()
 
 @then("Получаем таблицу с котировками из базы '{base_path}'")
 def step(context, base_path):
-    stock_price = read_sqlite_table(base_path)
-    context.stock_price = [(a,locale.atof(b)) for a,b in stock_price]
+    context.stock_price = parser.read_sqlite_table(base_path)
 
 @then("Получаем таблицу котировок цена которых изменилась на {per}%")
 def step(context, per):
