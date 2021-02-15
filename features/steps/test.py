611c865d239a6550544ef5e9e1b95058d38347bd
Dmitry Korzunin <kordimsan@mail.ru>
2021-02-15 08:34:03 +0500

    Получаем таблицу с котировками с сайта

--- features/steps/test.py
+++ features/steps/test.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+import locale
+import time
 from behave import *
 from selenium import webdriver
-import time
+
+locale.setlocale(locale.LC_ALL, 'nl_NL')
 
 option = webdriver.ChromeOptions()
 prefs = {'profile.default_content_setting_values': {'images':2, 'javascript':2}}
@@ -29,3 +32,13 @@ def step(context, name):
     elements = context.browser.find_elements_by_xpath("//section[@id='leftColumn']//h1")
     if not elements[0].get_attribute('innerText') == name:
         raise TextNotFound("Не найден заголовок '{name}'")
+
+@then("Получаем таблицу с котировками с сайта")
+def step(context):
+    elements = context.browser.find_elements_by_xpath("//table[@id='cross_rate_markets_stocks_1']//tbody//tr")
+    data = []
+    for tr in elements:
+        tds = tr.find_elements_by_tag_name('td')
+        if tds: 
+            data.append((tds[1].text,tds[2].text))
+    context.data = [(a,locale.atof(b)) for a,b in data]
